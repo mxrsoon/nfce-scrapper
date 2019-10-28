0b4a983b8696d793cf7e7936810c2600e3b10d4a
Marlon Macedo <me@wazy.me>
2019-10-27 21:40:04 -0300

    Removing puppeteer in favor of puppeteer code, now needs Chromium revision or path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 const scrappers = require("./scrappers");
 const helpers = require("./helpers");
 
@@ -6,6 +6,18 @@ let browser;
 
 async function init(options) {
     if (!browser || !browser.isConnected()) {
+        options = Object.assign({}, options);
+        
+        if (!options.executablePath) {
+            if (options.chromiumRev) {
+                const fetcher = puppeteer.createBrowserFetcher();
+                const revInfo = await fetcher.download(options.chromiumRev);
+                options.executablePath = revInfo.executablePath;
+            } else {
+                throw new Error("Must specify chromium executable path or revision to download");
+            }
+        }
+
         browser = await puppeteer.launch(options);
     }
 }
@@ -22,36 +34,43 @@ async function scrap(url, timeout = 0) {
 }
 
 async function dispose() {
-    if (browser && browser.isConnected()) {
+    if (browser) {
         await browser.close();
         browser = undefined;
     }
 }
 
 async function evaluateOnPage(url, func, timeout, ...args) {
-    const currentBrowser = browser || await puppeteer.launch();
-    
-    const page = await currentBrowser.newPage();
-    page.setDefaultNavigationTimeout(0);
-    page.setDefaultTimeout(0);
-    await page.goto(url, { timeout, waitUntil: "domcontentloaded" });
+    if (browser) {
+        const page = await browser.newPage();
+        await page.goto(url, { timeout, waitUntil: "domcontentloaded" });
 
-    const result = await page.evaluate(func, ...args);
-    await page.close();
+        const result = await page.evaluate(func, ...args);
+        await page.close();
 
-    if (currentBrowser !== browser) {
-        await currentBrowser.close();
+        return result;
+    } else {
+        throw new Error("Must call .init() first");
     }
-
-    return result;
 }
 
 async function main() {
-    const startTime = Date.now();
-    console.log(await scrap(process.argv[2]));
-    console.log(`\nSpent ${Date.now() - startTime}ms scrapping.`);
-    console.warn("Still running. Please, manually close.")
-    process.stdin.resume();
+    if (process.argv.length === 4) {
+        console.log("Setting Chromium up... This may take a while.");
+        await init({ chromiumRev: process.argv[3] });
+
+        const startTime = Date.now();
+        console.log("Scrapping... Please, wait.\n")
+        console.log(await scrap(process.argv[2]));
+        console.log(`\nSpent ${Date.now() - startTime}ms scrapping.`);
+        
+        await dispose();
+
+        console.warn("Still running. Please, manually close.")
+        process.stdin.resume();
+    } else {
+        throw new Error("Missing parameters: must pass QRCode's URL and Chromium revision to use");
+    }
 }
 
 if (!module || !module.parent) {
